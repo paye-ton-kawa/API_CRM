ce48e8b7d1eaacbab5928390f2204e2e7823dc1a
julien belinga <julienbelinga29@gmail.com>
2023-03-23 18:31:41 +0100

    test working
    @koalanoir need to add the Authorization in order to have all tests passing

--- src/test/routes/customer.test.js
+++ src/test/routes/customer.test.js
@@ -1,21 +1,21 @@
 const request = require('supertest');
-const routes = require('../../main/routes');
+const app = require('../../main/routes/customer');
 
 describe('Test the customer-related routes', () => {
   test('GET /api/private/customers should return a 200 status code and a message', async () => {
-    const response = await request(routes).get('/api/private/customers');
+    const response = await request(app).get('/api/private/customers');
     expect(response.status).toBe(200);
     expect(response.body.message).toBe("Hello from a public endpoint! You don't need to be authenticated to see this.");
   });
 
   test('GET /api/private/customers/:customerId/orders should return a 200 status code and a message', async () => {
-    const response = await request(routes).get('/api/private/customers/123/orders');
+    const response = await request(app).get('/api/private/customers/123/orders');
     expect(response.status).toBe(200);
     expect(response.body.message).toBe("Hello from a public endpoint! You don't need to be authenticated to see this.");
   });
 
   test('GET /api/private/customers/orders/:orderId/products should return a 200 status code and a message', async () => {
-    const response = await request(routes).get('/api/private/customers/orders/456/products');
+    const response = await request(app).get('/api/private/customers/orders/456/products');
     expect(response.status).toBe(200);
     expect(response.body.message).toBe("Hello from a public endpoint! You don't need to be authenticated to see this.");
   });
